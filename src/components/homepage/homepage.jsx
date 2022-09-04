1ac76617c92f258fe1ae7fa8e51e68f936a15383
Abed Khalaf <kfabdall@gmail.com>
2022-09-04 12:01:20 +0300

    add component

---
+++ src/components/homepage/homepage.jsx
@@ -0,0 +1,50 @@
+import "./homepage.css"
+function Homepage() {
+    return (
+      <div className="Homepage">
+        <div id="linkedin" class="linkedin">
+          <a href="https://www.linkedin.com/in/abedkhalaf7">linkedin</a>
+        </div>
+         <div id="github" class="github">
+         <a href="https://github.com/abedkhalaf8">github</a>
+         </div>
+        <div  id="resume"class="resume">
+          <a href="https://drive.google.com/file/d/1qppedvcTmvn5nn6CCaRFbaXassQLvzgg/view?usp=sharing">resume</a>
+          </div>
+        <div class="Group-1-outer">
+        <div
+          id="Group-1"
+          class="Group-1"
+        >
+          <div class="q1656620876158-1-outer">
+          <img
+            id="q1656620876158-1"
+            alt="1656620876158 1"
+            class="q1656620876158-1"
+          />
+          </div>
+          <div class="Hi--I-m-Abed-Khalaf--outer">
+          <div
+            id="Hi--I-m-Abed-Khalaf-"
+            class="Hi--I-m-Abed-Khalaf-"
+          ><div>Hi, I’m Abed Khalaf </div>
+<div key="1">And I’m a Frontend Developer </div>
+<div key="2">&nbsp;</div>
+<div key="3">&nbsp;</div>
+          </div>
+          </div>
+        </div>
+        </div>
+        <div class="About-me--I-m-very-m-outer">
+        <div
+          id="About-me--I-m-very-m"
+          class="About-me--I-m-very-m"
+        ><span class="About-me--I-m-very-m-span1">About me:<br /></span><span class="About-me--I-m-very-m-span2"> I’m very motivated for the frontend role because<br />it’s a changing field with exciting things to learn every day, also I love to work with the design things and things that the clients see. <br />what makes me unique for this role is that I’m skilled with the fronted technology with the ability to work fast and learn more <br />with creative and problem-solving skills. And my passion for tech comes from when was in fourth Grade, <br />I started to get into coding and discovering Linux systems after that high school<br />I got into a computer science class I learned C# and it was an 8-unit subject and I got a 99 grade.<br />this year I decided to join web dev Bootcamp during the Bootcamp I learned HTML, CSS, js, react, and typescript.</span>        </div>
+        </div>
+      </div>
+      
+    );
+  }
+  
+  export default Homepage;
+  
